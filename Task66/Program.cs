c5ada8826cc2682aa2a1856a382b437aab3151d3
ala markach <drozdowa1958@gmail.com>
2022-09-15 19:39:10 +0400

    add Task66 SumLoop M4,N8 = 30(4+5+6+7+8)

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumLoop(int M, int N)
+    {
+        if (N < M) return 0;
+        return N + SumLoop(M, N-1);
+    }
+try
+{
+    Console.WriteLine("Введите натуральные числа");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(SumLoop(m, n));
+}
+catch
+{
+    Console.WriteLine("Введите натуральное число!!!");
+}
+
